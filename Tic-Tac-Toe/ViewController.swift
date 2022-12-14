1afa07b7b3adec19fbcd793b8003785d3c0fda07
Andrei Pirogov Huertas <andrei.pirogov.huertas@gmail.com>
2022-12-14 02:56:11 +0100

    Added Score tracker and new function to set player scores and round winner

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -19,10 +19,23 @@ class ViewController: UIViewController {
     var row3Square2Button : String?
     var row3Square3Button : String?
     
+    @IBOutlet weak var playerXScoreNameLabel: UILabel!
+    
+    @IBOutlet weak var playerOScoreNameLabel: UILabel!
+    
+    
+    @IBOutlet weak var playerXScoreLabel: UILabel!
+    
+    @IBOutlet weak var playerOScoreLabel: UILabel!
+    
+    
     var playerTurn : String = "Player X"
     var playerX : String?
     var playerO : String?
     var roundWinner : String?
+    
+    var playerXScore : Int = 0
+    var playerOScore : Int = 0
     
     let segueToWinScreen = "SegueToWinScreen"
 
@@ -35,82 +48,106 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        //sets the initial player turn to player X
         PlayerTurnDisplayTracker(name: playerX ?? "Player X")
         
+        playerXScoreNameLabel.text = playerX
+        
+        playerOScoreNameLabel.text = playerO
+        
+        playerXScoreLabel.text = String(playerXScore)
+        
+        playerOScoreLabel.text = String(playerOScore)
         
         
     }
-    
+    // sends the winner name to winning screen and also both player names and score for a rematch
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == segueToWinScreen{
             if let destinationVC = segue.destination as? WinScreenViewController{
                 destinationVC.winnerName = roundWinner
                 destinationVC.playerX = playerX
                 destinationVC.playerO = playerO
+                destinationVC.playerXScore = playerXScore
+                destinationVC.playerOScore = playerOScore
             }
         }
     }
     
-    
+    // sets viewcontroller to winscreen
     func GoToWinScreen(){
         performSegue(withIdentifier: segueToWinScreen, sender: self)
     }
     
+    //sets the roundwinner for the winscreen and adds one point to the winner score
+    func SetRoundWinner(name: String){
+        roundWinner = "\(name) wins!"
+        if name == playerX{
+            playerXScore += 1
+        } else{
+            playerOScore += 1
+        }
+    }
+    
+    // Tracks all the win conditions for both players
+    //adds 1 score to winner
     func WinConditionTracker(){
         switch(row1Square1Button, row1Square2Button, row1Square3Button, row2Square1Button, row2Square2Button, row2Square3Button, row3Square1Button, row3Square2Button,row3Square3Button){
             
             //Win conditions for player X
+            //sets the name of the winner to be sent to the next viewcontroller
+            //goes to the next viewcontroller
         case("X","X", "X", _, _, _, _, _, _):
-            roundWinner = "\(playerX ?? "") wins!"
+          SetRoundWinner(name: playerX ?? "")
             GoToWinScreen()
-        case(_, _, _, "X", "X", "X", _, _, _): print("You Win!!")
-            roundWinner = "\(playerX ?? "") wins!"
+        case(_, _, _, "X", "X", "X", _, _, _):
+            SetRoundWinner(name: playerX ?? "")
             GoToWinScreen()
-        case(_, _, _, _, _, _, "X", "X", "X"): print("You Win!!")
-            roundWinner = "\(playerX ?? "") wins!"
+        case(_, _, _, _, _, _, "X", "X", "X"):
+            SetRoundWinner(name: playerX ?? "")
             GoToWinScreen()
-        case("X", _, _, "X", _, _, "X", _, _): print("You Win!!")
-            roundWinner = "\(playerX ?? "") wins!"
+        case("X", _, _, "X", _, _, "X", _, _):
+            SetRoundWinner(name: playerX ?? "")
             GoToWinScreen()
-        case(_, "X", _, _, "X", _, _, "X", _): print("You Win!!")
-            roundWinner = "\(playerX ?? "") wins!"
+        case(_, "X", _, _, "X", _, _, "X", _):
+            SetRoundWinner(name: playerX ?? "")
             GoToWinScreen()
-        case(_, _, "X", _, _, "X", _, _, "X"): print("You Win!!")
-            roundWinner = "\(playerX ?? "") wins!"
+        case(_, _, "X", _, _, "X", _, _, "X"):
+            SetRoundWinner(name: playerX ?? "")
             GoToWinScreen()
-        case("X", _, _, _, "X", _, _, _, "X"): print("You Win!!")
-            roundWinner = "\(playerX ?? "") wins!"
+        case("X", _, _, _, "X", _, _, _, "X"):
+            SetRoundWinner(name: playerX ?? "")
             GoToWinScreen()
-        case(_, _, "X", _, "X", _, "X", _, _): print("You Win!!")
-            roundWinner = "\(playerX ?? "") wins!"
+        case(_, _, "X", _, "X", _, "X", _, _):
+            SetRoundWinner(name: playerX ?? "")
             GoToWinScreen()
             
             //Win conditions for player O
             //sets the name of the winner to be sent to the next viewcontroller
             //goes to the next viewcontroller
-        case("O","O", "O", _, _, _, _, _, _): print("You Win!!")
-            roundWinner = "\(playerO ?? "") wins!"
+        case("O","O", "O", _, _, _, _, _, _):
+            SetRoundWinner(name: playerO ?? "")
             GoToWinScreen()
-        case(_, _, _, "O", "O", "O", _, _, _): print("You Win!!")
-            roundWinner = "\(playerO ?? "") wins!"
+        case(_, _, _, "O", "O", "O", _, _, _):
+            SetRoundWinner(name: playerO ?? "")
             GoToWinScreen()
-        case(_, _, _, _, _, _, "O", "O", "O"): print("You Win!!")
-            roundWinner = "\(playerO ?? "") wins!"
+        case(_, _, _, _, _, _, "O", "O", "O"):
+            SetRoundWinner(name: playerO ?? "")
             GoToWinScreen()
-        case("O", _, _, "O", _, _, "O", _, _): print("You Win!!")
-            roundWinner = "\(playerO ?? "") wins!"
+        case("O", _, _, "O", _, _, "O", _, _):
+            SetRoundWinner(name: playerO ?? "")
             GoToWinScreen()
-        case(_, "O", _, _, "O", _, _, "O", _): print("You Win!!")
-            roundWinner = "\(playerO ?? "") wins!"
+        case(_, "O", _, _, "O", _, _, "O", _):
+            SetRoundWinner(name: playerO ?? "")
             GoToWinScreen()
-        case(_, _, "O", _, _, "O", _, _, "O"): print("You Win!!")
-            roundWinner = "\(playerO ?? "") wins!"
+        case(_, _, "O", _, _, "O", _, _, "O"):
+            SetRoundWinner(name: playerO ?? "")
             GoToWinScreen()
-        case("O", _, _, _, "O", _, _, _, "O"): print("You Win!!")
-            roundWinner = "\(playerO ?? "") wins!"
+        case("O", _, _, _, "O", _, _, _, "O"):
+            SetRoundWinner(name: playerO ?? "")
             GoToWinScreen()
-        case(_, _, "O", _, "O", _, "O", _, _): print("You Win!!")
-            roundWinner = "\(playerO ?? "") wins!"
+        case(_, _, "O", _, "O", _, "O", _, _):
+            SetRoundWinner(name: playerO ?? "")
             GoToWinScreen()
             
             //Condition where no player wins
@@ -125,6 +162,7 @@ class ViewController: UIViewController {
         
     }
 
+    //displays the name of the current player turn
     func PlayerTurnDisplayTracker(name : String){
         playerTurnLabel.text = "Current Turn: \(name)"
     }
@@ -132,12 +170,16 @@ class ViewController: UIViewController {
     
     
     func PlayerMarksSpot(sender: UIButton){
+        //sets the font and disables the pressed button
         sender.titleLabel?.font = UIFont(name: "Helvetica", size: 80)
         sender.isEnabled = false
         
+        //checks what player pressed
         if playerTurn == "Player O"{
             sender.setTitle("O", for: .normal)
+            //changes whose turn it displays
             PlayerTurnDisplayTracker(name: playerX ?? "Player X")
+            // sets whose turn it is to set a different marker
             playerTurn = "Player X"
         }else if playerTurn == "Player X"{
             sender.setTitle("X", for: .normal)
@@ -150,7 +192,9 @@ class ViewController: UIViewController {
         
     }
     
-    
+    // when a button is pressed it is marked depending on the current player turn
+    // the button variable is given a value
+    // checks if win conditions are achieved
     @IBAction func row1Square1Pressed(_ sender: UIButton) {
         PlayerMarksSpot(sender: sender)
         
--- Tic-Tac-Toe/WinScreenViewController.swift
+++ Tic-Tac-Toe/WinScreenViewController.swift
@@ -11,27 +11,47 @@ class WinScreenViewController: UIViewController {
 
     @IBOutlet weak var winnerNameLabel: UILabel!
     
+    @IBOutlet weak var playerXScoreNameLabel: UILabel!
+    
+    @IBOutlet weak var playerOScoreNameLabel: UILabel!
+    
+    @IBOutlet weak var playerXScoreLabel: UILabel!
+    
+    @IBOutlet weak var playerOScoreLabel: UILabel!
+    
+    
     var segueToRematch = "SegueToRematch"
     
     var winnerName : String?
     var playerX : String?
     var playerO : String?
     
+    var playerXScore : Int?
+    var playerOScore : Int?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //displays the right winner name
         winnerNameLabel.text = winnerName ?? ""
         
+        //displays the correct player names and scores
+        playerXScoreNameLabel.text = playerX
+        playerOScoreNameLabel.text = playerO
+        playerXScoreLabel.text = String(playerXScore ?? 0)
+        playerOScoreLabel.text = String(playerOScore ?? 0)
         
     }
     
-// Sends the player names back for a rematch
+// Sends the player names and score back for a rematch
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == segueToRematch{
             if let destinationVC = segue.destination as? ViewController{
                 destinationVC.playerX = playerX
                 destinationVC.playerO = playerO
+                destinationVC.playerXScore = playerXScore ?? 0
+                destinationVC.playerOScore = playerOScore ?? 0
             }
         }
     }
